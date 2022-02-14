0b3561ef53bbf26f37b4a08465c9144dc0d97a23
Алексей <54481639+tayrenov@users.noreply.github.com>
2022-02-14 18:13:20 +0300

    Delete config directory

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports ={
-    mongoURI: 'ter0-shard-00-01.sugar.mongodb.net:27017,cluster0-shard-00-02.sugar.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-v7gufr-shard-0&authSource=admin&retryWrites=true&w=majority'
-}
