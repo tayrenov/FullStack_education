a1e29cd3e05e37a260a72b30707c3729f40e9d12
Aleksey <tayrenov@gmail.com>
2022-02-15 14:43:46 +0300

    token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,36 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs'),
+      jwt = require('jsonwebtoken');
 
-const User = require('../models/User')
+const User = require('../models/User'),
+      keys = require('../config/keys')
 
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+
+        if (passwordResult) {
+
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            },keys.jwt, {expiresIn: 60 * 60})
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message: 'Пароли не совпадают'
+            })
         }
-    })
+
+    } else {
+        res.status(404).json({
+            message: 'ПОльзователь с таким email не найден'
+        })
+    }
 }
 
 module.exports.register = async function(req, res) {
