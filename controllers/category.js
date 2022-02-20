e655e8fbddf23d0c9666a0fa846ef155c5ac8f55
Aleksey <tayrenov@gmail.com>
2022-02-21 00:25:06 +0300

    add get and delete categories

--- controllers/category.js
+++ controllers/category.js
@@ -1,13 +1,35 @@
-module.exports.getAll = function(req, res) {
-    res.json({message: 'Yep'})
+const Category = require('../models/Category'),
+      Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getAll = async function(req, res) {
+    try {
+        const categories = await Category.find({user: req.user.id})
+        res.status(200).json(categories)
+    } catch (e) {
+        errorHandler(e)
+    }
 }
 
-module.exports.getById = function(req, res) {
-    
+module.exports.getById = async function(req, res) {
+    try {
+        const category = await Category.findById(req.params.id)
+        res.status(200).json(category)
+    } catch (e) {
+        errorHandler(e)
+    }
 }
 
-module.exports.remove = function(req, res) {
-    
+module.exports.remove = async function(req, res) {
+    try {
+        await Category.remove({_id: req.params.id})
+        await Position.remove({category: req.params.id})
+        res.status(200).json({
+            message: 'Категория удалена.'
+        })
+    } catch (e) {
+        errorHandler(e)
+    }
 }
 
 module.exports.create = function(req, res) {
